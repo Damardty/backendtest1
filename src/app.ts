63625147da8ac2710a8f94757f8ed88c92d467f1
leonardmarsss <leonardmars63@gmail.com>
2020-10-31 11:49:33 +0700

    heheheheh

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,18 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import http from 'http';
 import config from './environment';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors({origin: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+routes(app);
 
 const server =  http.createServer(app);
 
+/*
 app.get('/index', (req, res) => {
     console.log("req: ", req)
     return res.status(200).json({message: "success!!!"});
@@ -31,6 +34,7 @@ app.put('/update', (req, res) => {
     console.log("req: ", req)
     return res.status(200).json({message: "success!!!"});
 })
+*/
 
 
 function startServer(){
