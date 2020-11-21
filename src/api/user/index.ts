aad44f85e872b7f12eb3c7905870aaabcb2627cb
leonardmarsss <leonardmars63@gmail.com>
2020-11-21 12:06:11 +0700

    week x

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import {
-    index, create, remove, update
+    index, register, signin, remove, update
 } from './user.controller';
+import * as middleware from '../../middleware';
 
 let userRouter = express.Router();
 
-userRouter.get('/', index);
-userRouter.post('/', create);
-userRouter.delete('/', remove);
-userRouter.put('/', update);
+userRouter.get('/', middleware.isAuthenticated, index);
+userRouter.post('/register', register);
+userRouter.post('/signin', signin);
+userRouter.delete('/:id', middleware.isAuthenticated, remove);
+userRouter.put('/:id', middleware.isAuthenticated, update);
 
 export default userRouter;
